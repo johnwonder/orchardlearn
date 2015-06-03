1cd713621c42b5a1a6a0296ca950eb365660cd77
johnwonder <johnufida@163.com>
2015-06-03 09:19:37 +0200

    增加HelloWorld Controller

--- orchard/src/Orchard.Web/Modules/Mycompany.Helloworld/Controllers/HomeController.cs
+++ orchard/src/Orchard.Web/Modules/Mycompany.Helloworld/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
 using System.Web.Mvc;
 using Orchard.Localization;
 using Orchard;
+using Orchard.Themes;
 
 namespace MyCompany.Helloworld.Controllers {
+    
+    /// <summary>
+    /// 添加一个Themed的属性，否则所呈现的内容不会在Orchard的皮肤中显示
+    /// </summary>
+    [Themed]
     public class HomeController : Controller {
         public IOrchardServices Services { get; set; }
 
         public HomeController(IOrchardServices services) {
             Services = services;
             T = NullLocalizer.Instance;
+        }
+
+        public ActionResult Index()
+        {
+            string model = "Hello World";
+            return View((object)model);
         }
 
         public Localizer T { get; set; }
---
+++ orchard/src/Orchard.Web/Modules/Mycompany.Helloworld/Routes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Mvc.Routes;
+using System.Web.Routing;
+using System.Web.Mvc;
+
+namespace Mycompany.Helloworld
+{
+    /// <summary>
+    /// 定义模块所用到的路由，在Orchard中定义路由需实现IRouteProvider接口
+    /// </summary>
+    public class Routes:IRouteProvider
+    {
+
+        public IEnumerable<RouteDescriptor> GetRoutes()
+        {
+            return new[]{
+             
+                    new RouteDescriptor{
+                    
+                          Priority = 5,
+                          Route = new Route(
+                              
+                                "Helloworld",
+                                new RouteValueDictionary{
+                                
+                                        {"area","MyCompany.HelloWorld"},
+                                        {"controller","Home"},
+                                        {"action","Index"}
+                                },
+                                new RouteValueDictionary(),
+                                new RouteValueDictionary { { "area","MyCompany.HelloWorld" } },
+                                new MvcRouteHandler()
+                              
+                              )
+                    
+                    }
+             
+             };
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes)
+        {
+            foreach (var routeDescriptor in GetRoutes())
+            {
+                routes.Add(routeDescriptor);
+            }
+        }
+    }
+}
